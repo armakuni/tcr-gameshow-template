5d9262a206c8a1ca3fa698974bfd25ec04437293
Ben Nagy <nagy.benb@gmail.com>
2023-08-11 14:22:28 +0100

    update python template repo

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. Clone this repo
 1. Run `setup.sh`
 1. Run `source env.sh`
-1. Set your programming language `TCR_LANG=python`
+1. Set your programming language e.g. `TCR_LANG=python`
+1.
--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [ -z "$TCR_LANG" ]; then
+  echo "You need to set the language you're using"
+  echo "Example: export TCR_LANG=python"
+  echo "Example: export TCR_LANG=typescript"
+  exit 1
+fi
+
 git remote remove origin > /dev/null 2>&1
+
+echo "Setting up $TCR_LANG environment..."
+
+(cd $TCR_LANG && ./setup.sh)
