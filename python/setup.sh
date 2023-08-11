e443244dcc40d181bb5685e41254fe1bd9e1e78f
Jonathan Acker <jon.acker@armakuni.com>
2023-08-11 15:27:34 +0100

    verbose on python version

--- python/setup.sh
+++ python/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python -m venv .venv || python3 -m venv .venv
+python -m venv .venv || (echo "Trying python3 instead" && python3 -m venv .venv)
 .venv/bin/python -m pip install -r requirements.txt
